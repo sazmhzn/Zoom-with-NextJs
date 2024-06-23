cda501baafba872bc0b2a9f1601f4c6abd2e1196
suron <suron.maharjan@gmail.com>
2024-06-23 11:39:00 +0545

    add Home page design

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const protectedRoutes = createRouteMatcher([
+  '/',
+  '/upcoming',
+  '/previous',
+  '/recordings',
+  '/personal-room',
+  '/meeting(.*)'
+])
+
+const isIgnoredRoute = createRouteMatcher([
+  '/sign-in',
+  '/sign-up'
+])
+
+export default clerkMiddleware( (auth, req) => {
+  // if(!isIgnoredRoute(req)) auth().protect();
+  if(protectedRoutes(req)) auth().protect();
+} );
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
